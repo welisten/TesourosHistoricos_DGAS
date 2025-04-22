c9367b01d6b849a5e39c6f47de5fbca45a78942b
welisten <welistenw@gmail.com>
2025-04-22 15:55:41 -0300

    DGAS [G3] - add vLibras and app proxy middlewares with route rewrite to /app/target

--- middlewares/vLibrasProxyMiddleware.js
+++ middlewares/vLibrasProxyMiddleware.js
@@ -15,13 +15,15 @@
 require('dotenv').config()
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
-const PORT = process.env.PORT || 3333
+const PORT = process.env.PORT || 9999
+
+
 
 const vLibrasproxyMiddleware = createProxyMiddleware({
     target: 'https://cdn.jsdelivr.net/gh/spbgovbr-vlibras/vlibras-portal@dev', // URL do servidor externo
     changeOrigin: true,
     pathRewrite: {
-      [`http://localhost:${PORT}/app/target`]: 'https://cdn.jsdelivr.net/gh/spbgovbr-vlibras/vlibras-portal@dev/app/target', // Remove o prefixo /api da URL e ajusta o caminho
+      '.*(/app/target)$' : 'https://cdn.jsdelivr.net/gh/spbgovbr-vlibras/vlibras-portal@dev/app/target', // Remove o prefixo /api da URL e ajusta o caminho
     },
     on:{
         proxyReq: (proxyReq, req, res) => {
